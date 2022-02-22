89c3ba36c96f85be33ed9a525403f10a19666a63
MuriloM <muriloommaia@gmail.com>
2022-02-22 14:58:55 -0300

    conexao com banco de dados

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,14 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import db from './db/connection';
+import { IUser } from './interfaces/IUser';
 
 const app = express();
-
 app.use(express.json());
+
+app.get('/', async (req: Request, res: Response) => {
+  const allItems:IUser[] = await db.select('*').from('Users');
+  console.log(allItems);
+  res.status(200).json(allItems);
+});
 
 export default app;
---
+++ src/db/connection.ts
@@ -0,0 +1,17 @@
+import knex from 'knex';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const db = knex({
+  client: 'mysql2',
+  connection: {
+    host: process.env.MYSQL_HOST,
+    port: 3306,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: 'Trybesmith',
+  },
+});
+
+export default db;
